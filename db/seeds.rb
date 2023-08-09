fe3ae9a59a4a1c01cc6dcad2fb46127ce3c3c3fc
Varsion <jianhual12@outlook.com>
2023-08-09 23:17:29 +0800

    feat: update seed to create default books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do |i|
+  Book.create(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    price: 100,
+  )
+end
