32d240c79e3b3e0327ce8a1f3b0414bd1a554648
Varsion <jianhual12@outlook.com>
2023-08-10 17:37:17 +0800

    feat: account amount and frozen_amount check

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,12 @@ class Account < ApplicationRecord
   has_many :transactions
 
   has_many :due_transactions, -> { where(status: :returned) }, class_name: "Transaction", foreign_key: :account_id
+
+  validate :check_amount
+
+  def check_amount
+    if amount < 0 || frozen_amount < 0
+      errors.add(:amount, "Calculation or Service error")
+    end
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    it "update amount to -10, get errors" do
+      account = create(:account)
+      account.update(amount: -10)
+      expect(account.errors).not_to be(nil)
+    end
+
+    it "create amount to -10, get errors" do
+      account = Account.create(name: "Jianhua", amount: -10)
+      expect(account.errors).not_to be(nil)
+    end
+  end
 end
