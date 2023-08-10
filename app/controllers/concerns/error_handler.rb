0ecc037207f584ea901b7ce6050a033220474029
Varsion <jianhual12@outlook.com>
2023-08-10 17:55:58 +0800

    feat: rescue the transaction errors

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -3,6 +3,7 @@ module ErrorHandler
 
   included do
     rescue_from(ActiveRecord::RecordNotFound) { |err| handle_404(err: err) }
+    rescue_from(ActiveRecord::RecordInvalid) { |err| handle_record_invalid(err: err) }
   end
 
   def handel_error(status:, err: nil, message:, fields: nil)
@@ -21,14 +22,17 @@ module ErrorHandler
     handel_error(status: 404, err: err, message: message, fields: fields)
   end
 
+  def handle_record_invalid(err:)
+    handel_error(status: 400, err: err, message: err.message || "Bad Request", fields: err.class)
+  end
+
   def handle_active_record_validation_errors(errors: nil, status: 422)
     result = errors.map do |err|
       {
         field: err.attribute,
-        message:err.type
+        message: err.type,
       }
     end
-    debugger
     render json: result, status: status
   end
 end
